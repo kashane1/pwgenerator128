e90304c05a9c6d18ee5703fc292f777235e39ef4
Kashane S <ksakhakorn@gmail.com>
2022-01-12 18:44:05 -0800

    committing all the files at once. will update a few more times before submitting.

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# pwgenerator128
-pwgenerator128
+Password Generator
+=======
+[Link](https://kashane1.github.io/portfolio/)
+-----------
+Following the link above will bring you to the password generator.
+
+-----------
+Requirements:
+-----------
+
+1. My portfolio contains my name, a recent photo, and links to sections about me, my work, and how to contact me.
+2. The navigation links to corresponding sections on the page
+3. My work has project titles with images of the applications (placeholders for now)
+4. My first application is larger than the others
+5. Each image of the application can be clicked on to deploy that application
+6. The page and be resized and the layout will adapt to the viewport
+
+## User Story
+
+```
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+```
+
+## Acceptance Criteria
+
+```
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+-----------
+Process:
+-----------
+My original intention was to make a bare bones webpage for my portfolio and then update it in the following months. But I enjoyed the process too much and wanted to fill out the whole page as best as I could. I still have plenty more to add, but for now my portfolio gives a good teaser of my software engineering capabailities. 
+
+-----------
+Extra:
+-----------
+1. I tried to incorporate new css that we learned as well as javascript
+2. I added a fun easter egg or two in there!
+
+-----------
+```
+preview of my portfolio:
+```
+![Image](./assets/images/screenshot3.jpg "screenshot of my portflio")
+
+-----------
+Cited:
+-----------
+https://www.w3schools.com/ - references to html and css
+
+https://css-tricks.com/snippets/css/a-guide-to-flexbox/ - css flex box help
+
+https://favicon.io/ - favicon generator
+
+-----------
---
+++ script.js
@@ -0,0 +1,91 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  //creating necessary variables and prompting the user when doing so
+  var pwLength = window.prompt("Please choose the length of your password.\n\n(It must be from 8 to 128 characters)", 8);
+  while (pwLength < 8 || pwLength > 128) {
+    pwLength = window.prompt("Please choose a length between 8 and 128.\n\n", 8);
+  }
+  
+  var isLowercase = window.confirm("Would you like to include the following type of character?\n\n(You must include at least 1 of the 4 types)\n\n  1. lowercase letters");
+  var isUppercase = window.confirm("Would you like to include the following type of character?\n\n(You must include at least 1 of the 4 types)\n\n  2. uppercase letters");
+  var isNumbers = window.confirm("Would you like to include the following type of character?\n\n(You must include at least 1 of the 4 types)\n\n  3. numbers: 0-9");
+  var isSpecial = window.confirm("Would you like to include the following type of character?\n\n(You must include at least 1 of the 4 types)\n\n  4. special characters: ! @ # $ % ^ & * ( ) _ + - =");
+  
+  //special use case for if they didnt select any character type, needed to prompt them over again
+  while (!isLowercase && !isUppercase && !isNumbers && !isSpecial) {
+    window.alert("Sorry, you must select at least 1 of the 4 character types:\n\n 1. lowercase numbers\n\n  2. uppercase letters\n\n  3. numbers\n\n  4. special characters");
+    isLowercase = window.confirm("Would you like to include the following type of character?\n\n(You must include at least 1 of the 4 types)\n\n  1. lowercase letters");
+    isUppercase = window.confirm("Would you like to include the following type of character?\n\n(You must include at least 1 of the 4 types)\n\n  2. uppercase letters");
+    isNumbers = window.confirm("Would you like to include the following type of character?\n\n(You must include at least 1 of the 4 types)\n\n  3. numbers: 0-9");
+    isSpecial = window.confirm("Would you like to include the following type of character?\n\n(You must include at least 1 of the 4 types)\n\n  4. special characters: ! @ # $ % ^ & * ( ) _ + - =");
+  }
+
+  //combing the users choices into a more readable format to show them later
+  var charChoice = "";
+  if (isLowercase) {
+    charChoice += "lowercase letters\n"
+  }
+  if (isUppercase) {
+    charChoice += "upercase letters\n"
+  }
+  if (isNumbers) {
+    charChoice += "numbers\n"
+  }
+  if (isSpecial) {
+    charChoice += "special characters\n"
+  }
+  
+
+  //confirming with the user the parameters that they chose for their pw, and asking to initiate the pw generator
+  var start = window.confirm("You have chosen to generate a password that is " + pwLength + " characters long.\n\nIt will include:\n" + charChoice + "\nDo you want to continue with generating your password?")
+
+  //these arrays hold all the values of each specific character type that will be used when generating the pw
+  var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+  var nums = ["0","1","2","3","4","5","6","7","8","9"]
+  var spec = ["!","@","#","$","%","^","&","*","(",")","_","+","-","="]
+
+  //this new array will concatinate the users choices of each character type
+  var allCharacters = [];
+  if (isLowercase) {
+    allCharacters = allCharacters.concat(lower);
+  }
+  if (isUppercase) {
+    allCharacters = allCharacters.concat(upper);
+  }
+  if (isNumbers) {
+    allCharacters = allCharacters.concat(nums);
+  }
+  if (isSpecial) {
+    allCharacters = allCharacters.concat(spec);
+  }
+
+  console.log(allCharacters);
+  
+  var password = [];
+  var newChar;
+  
+  for (var i = 0; i < pwLength; i++) {
+    newChar = allCharacters[Math.floor(Math.random() * allCharacters.length)]
+    console.log(newChar);
+    password = password.concat(newChar);
+    console.log(password)
+  }
+
+  //console.log(password.join(""));
+  return password.join("");
+}
